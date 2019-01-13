d547169de826166abb56f17053dae1251649751c
yjwfn <xyjwfn@gmail.com>
2019-01-14 00:24:21 +0800

    Add docker-compose.yaml

--- AppBubbleSmsService/src/main/java/com/bubble/sms/SmsApplication.java
+++ AppBubbleSmsService/src/main/java/com/bubble/sms/SmsApplication.java
@@ -1,10 +1,14 @@
 package com.bubble.sms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableRedisRepositories
 public class SmsApplication {
 

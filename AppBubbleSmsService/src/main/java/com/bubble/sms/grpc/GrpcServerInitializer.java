2ef47f274916fce6d028fd8aa539d438d69e070d
yjwfn <liuwei05288@hellobike.com>
2019-01-14 20:00:57 +0800

    Init gRPC

--- AppBubbleSmsService/src/main/java/com/bubble/sms/grpc/GrpcServerInitializer.java
+++ AppBubbleSmsService/src/main/java/com/bubble/sms/grpc/GrpcServerInitializer.java
@@ -4,7 +4,6 @@ import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -18,7 +17,7 @@ public class GrpcServerInitializer implements ApplicationRunner {
     @Autowired
     private List<BindableService> services;
 
-//    @Value("#{server.port}")
+    //    @Value("#{server.port}")
     private int port = 8090;
 
     @Override
@@ -27,13 +26,27 @@ public class GrpcServerInitializer implements ApplicationRunner {
         ServerBuilder serverBuilder = ServerBuilder
                 .forPort(port);
 
-        if(services != null && !services.isEmpty()){
-            for(BindableService bindableService: services){
+        if (services != null && !services.isEmpty()) {
+            for (BindableService bindableService : services) {
                 serverBuilder.addService(bindableService);
             }
         }
 
         Server server = serverBuilder.build();
         server.start();
+        startDaemonAwaitThread(server);
+    }
+
+
+    private void startDaemonAwaitThread(Server server) {
+        Thread awaitThread = new Thread(() -> {
+            try {
+                server.awaitTermination();
+            } catch (InterruptedException e) {
+//                log.error("gRPC server stopped.", e);
+            }
+        });
+        awaitThread.setDaemon(false);
+        awaitThread.start();
     }
 }

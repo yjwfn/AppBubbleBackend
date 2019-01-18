312fb87ba27ab72b72e2556a7ce67872f391afcc
yjwfn <xyjwfn@gmail.com>
2019-01-18 08:46:21 +0800

    添加envoy代理

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -5,52 +5,22 @@ export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
 rootProjectDir="/app"
 projectDir="${rootProjectDir}/${APP_PROJECT_NAME}"
-gatewayProjectDir="${rootProjectDir}/gateway"
+descriptorSetsFile=${projectDir}/target/generated-resources/protobuf/descriptor-sets/app.protobin
+shareProtobufVolumePath=/app/protobuf
 
+
+cd ${rootProjectDir}/AppBubbleCommon
+./mvnw install
 
 cd $projectDir
 #打包app.jar
 ./mvnw package -DskipTests -P docker-build
 #编译proto文件
-./mvnw protobuf:compile protobuf:compile-custom
+./mvnw protobuf:compile protobuf:compile-custom -P docker-build
 
+# 复制Descriptor文件到共享目录
+cp $descriptorSetsFile $shareProtobufVolumePath/${APP_PROJECT_NAME}.protobin
 
-# Generate gRPC stub
-protosDir=${rootProjectDir}/protos
-# 服务目录
-serviceProtoDirs=$(ls $protosDir)
-# 创建输出目录
-protoOutput=${gatewayProjectDir}
-mkdir -p ${protoOutput}
-
-
-for serviceDir in $serviceProtoDirs
-do
-    # 需要绝对路径，相对路径protoc会报重复定义错误
-    protoFiles=$(ls $protosDir/$serviceDir | sed "s:\(.*\):$protosDir/$serviceDir/\1:")
-
-    protoc \
-      -I/usr/local/include \
-      -I${protosDir} \
-      -I$GOPATH/src \
-      -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-      --go_out=plugins=grpc:${GOPATH}/src \
-      ${protoFiles}
-
-    protoc \
-      -I/usr/local/include \
-      -I${protosDir} \
-      -I$GOPATH/src \
-      -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-      --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
-      ${protoFiles}
-done
-
-
-# Run gateway
-cd ${gatewayProjectDir}
-sed -i -e "s/\${APP_PROTO_PACKAGE}/${APP_PROTO_PACKAGE}/;s/\${APP_GATEWAY_SERVICE}/${APP_GATEWAY_SERVICE}/" app.go
-go run app.go &
 
 # Run service
 java -jar ${projectDir}/target/app.jar

e5b565a05fb0ae246355769147fed58c659e32c3
yjwfn <xyjwfn@gmail.com>
2019-01-19 20:07:54 +0800

    实现注册用户接口

--- AppBubbleUserService/src/main/java/com/bubble/user/grpc/GrcpUserServiceImpl.java
+++ AppBubbleUserService/src/main/java/com/bubble/user/grpc/GrcpUserServiceImpl.java
@@ -5,6 +5,7 @@ import com.bubble.sms.grpc.user.service.UserServiceGrpc;
 import com.bubble.user.dto.user.PhoneRegistryDto;
 import com.bubble.user.dto.user.UserDto;
 import com.bubble.user.service.UserService;
+import com.google.common.base.Strings;
 import io.grpc.stub.StreamObserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,22 +22,72 @@ public class GrcpUserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
         PhoneRegistryDto phoneRegistryDto = new PhoneRegistryDto(request.getPhoneExt(), request.getPhone(), request.getPassword(), request.getToken(), request.getVerificationCode());
         UserDto userDto = userService.registerWithPhone(phoneRegistryDto);
 
-        UserMessageProto.UserProfile userProfile = UserMessageProto.UserProfile.newBuilder()
-                .setId(userDto.getId())
-                .setNickName(userDto.getNickName())
-                .setCreateTime(userDto.getCreateTime())
-                .setAvatarUrl(userDto.getAvatarUrl())
-                .setBirthday(userDto.getBirthday())
-                .setCountry(userDto.getCountry())
-                .setState(userDto.getState())
-                .setEmail(userDto.getEmail())
-                .setCity(userDto.getCity())
-                .setGender(userDto.getGender())
-                .setPhoneExt(userDto.getPhoneExt())
-                .setPhone(userDto.getPhone())
-                .build();
+        UserMessageProto.UserProfile.Builder userProfileBuilder = UserMessageProto.UserProfile.newBuilder();
 
-        responseObserver.onNext(userProfile);
+        if(userDto.getId() != null){
+            userProfileBuilder.setId(userDto.getId());
+        }
+
+        if(!Strings.isNullOrEmpty(userDto.getNickName())){
+            userProfileBuilder.setNickName(userDto.getNickName());
+        }
+
+
+        if(userDto.getCreateTime() != null){
+            userProfileBuilder.setCreateTime(userDto.getCreateTime());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getAvatarUrl())){
+            userProfileBuilder.setAvatarUrl(userDto.getAvatarUrl());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getNickName())){
+            userProfileBuilder.setNickName(userDto.getNickName());
+        }
+
+
+        if(userDto.getBirthday() != null){
+            userProfileBuilder.setBirthday(userDto.getBirthday());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getCountry())){
+            userProfileBuilder.setCountry(userDto.getCountry());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getState())){
+            userProfileBuilder.setState(userDto.getState());
+        }
+
+        if(!Strings.isNullOrEmpty(userDto.getEmail())){
+            userProfileBuilder.setEmail(userDto.getEmail());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getCity())){
+            userProfileBuilder.setCity(userDto.getCity());
+        }
+
+
+        if(userDto.getGender() != null){
+            userProfileBuilder.setGender(userDto.getGender());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getPhoneExt())){
+            userProfileBuilder.setPhoneExt(userDto.getPhoneExt());
+        }
+
+
+        if(!Strings.isNullOrEmpty(userDto.getPhone())){
+            userProfileBuilder.setPhone(userDto.getPhone());
+        }
+
+
+        responseObserver.onNext(userProfileBuilder.build());
         responseObserver.onCompleted();
     }
 }
